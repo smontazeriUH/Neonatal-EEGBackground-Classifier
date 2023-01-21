87abe4c38ddda9964b65621e8731b4076cab1900
Saeed Montazeri <62646318+smontazeriUH@users.noreply.github.com>
2023-01-21 12:40:46 +0200

    Update preprocess.m

--- Preprocessing/preprocess.m
+++ Preprocessing/preprocess.m
@@ -3,7 +3,7 @@
 %
 % Syntax: [eeg_data,ArtifactPercPerCh,channelList] = preproces(dat,fs,scle,offs,labels,fs_new,ElectricFreq)
 %
-% Inputs: 
+% Inputs:
 %     dat               - Raw EEG data
 %     fs                - sampling frequency
 %     scle              - scale of signal
@@ -12,104 +12,140 @@
 %     fs_new            - resampling frequency
 %     ElectricFreq      - electricity frequency
 %
-% Outputs: 
+% Outputs:
 %     eeg_data          - preprocessed signal
 %     ArtifactPercPerCh - proportion of detected artifacts per channel
 %     channelList       - list of channels and couplings
 %
 % Saeed Montazeri M., University of Helsinki
 % Started: 10-11-2019
+% Updated on 13-01-2023 to become compatiable with Tampere dataset
 %-------------------------------------------------------------------------------
-function [eeg_data,ArtifactPercPerCh,channelList] = preprocess(dat,fs,scle,offs,labels,fs_new,ElectricFreq)
+function [eeg_data,artefact_ratePerCh,channelList] = preprocess(dat,fs,scle,offs,labels,fs_new,ElectricFreq)
+
+% indicate usefull EEG channels and remove mastoids
+% based on international 10-20 system
+leads = {'Fp1','Fp2','F3','F4','F7','F8','C3','C4','T3',...
+    'T4','P3','P4','T5','T6','O1','O2','A1','A2','Fpz','Fz','Cz','Pz'};
+
+% remove "EEG" from the begining of labels
+for ich = 1 : length(labels)
+    if strncmp(labels{ich},'EEG ',4)
+        labels{ich} = labels{ich}(5:end);
+    end
+end
+
+% Make montages and preprocess
+[eeg_data, artefact_ratePerCh, channelList] = make_montages_preprocess(leads, ...
+    labels, dat, scle, offs, fs, fs_new, ElectricFreq);
+
+end
+
+function [eeg_data, artefact_ratePerCh, channelList] = make_montages_preprocess(leads, ...
+    labels, dat, scle, offs, fs, fs_new, ElectricFreq)
+eeg_data = [];
+artefact_ratePerCh = [];
+channelList = cell(0);
+for ich = 1 : length(leads)
+
+    fch = find(strncmp(labels,leads{ich},length(leads{ich})));
+    if ~isempty(fch)
+
+        for ifch = 1 : length(fch)
+
+            if contains(labels{fch(ifch)}.','-')
+
+                if ~contains(labels{fch(ifch)}.','REF') && ~contains(labels{fch(ifch)}.','ref') &&...
+                        ~contains(labels{fch(ifch)}.','Ref')
+
+                    % bipolar
+                    raw_data = double(dat{fch(ifch)}).*scle(fch(ifch))-offs(fch(ifch)); %Scaling the signal to microvolts
+                    [eeg_tmp, art_tmp] = preprocess_run(raw_data,fs,fs_new,ElectricFreq,1);
+                    eeg_data = [eeg_data; eeg_tmp];
+                    artefact_ratePerCh = [artefact_ratePerCh; art_tmp];
+                    channelList = [channelList, labels{fch(ifch)}];
+                    clear eeg_tmp art_tmp raw_data
+                else
+
+                    % monopolar
+                    raw_data = double(dat{fch(ifch)}).*scle(fch(ifch))-offs(fch(ifch)); %Scaling the signal to microvolts
+                    [eeg_tmp, art_tmp] = preprocess_run(raw_data,fs,fs_new,ElectricFreq,0);
+                    eeg_data = [eeg_data; eeg_tmp];
+                    artefact_ratePerCh = [artefact_ratePerCh; art_tmp];
+                    channelList = [channelList, labels{fch(ifch)}];
+                    clear eeg_tmp art_tmp raw_data
+                end
+
+            else
+                % monopolar
+                if ~strncmp('A1',labels{fch(ifch)}.',2) && ~strncmp('A2',labels{fch(ifch)}.',2) && ...
+                        ~strncmp('Fpz',labels{fch(ifch)}.',3) && ~strncmp('Fz',labels{fch(ifch)}.',2) &&...
+                        ~strncmp('Cz',labels{fch(ifch)}.',2) && ~strncmp('Pz',labels{fch(ifch)}.',2)
+
+                    raw_data = double(dat{fch(ifch)}).*scle(fch(ifch))-offs(fch(ifch)); %Scaling the signal to microvolts
+                    [eeg_tmp, art_tmp] = preprocess_run(raw_data,fs,fs_new,ElectricFreq,0);
+                    eeg_data = [eeg_data; eeg_tmp];
+                    artefact_ratePerCh = [artefact_ratePerCh; art_tmp];
+                    channelList = [channelList, labels{fch(ifch)}];
+                    clear eeg_tmp art_tmp raw_data
+                end
+            end
+        end
+    end
+end
+end
+
+function [eeg_data, artefact_rate] = preprocess_run(raw_data,fs,fs_new,ElectricFreq,FLGbipolar)
+%% preprocessing per channel
 
 % Notch filter
 [bn, an] = FilterNotch(fs,ElectricFreq);
 % BandPass filter
 [bb, ab] = FilterBandPass(fs);
 
-% indicate usefull EEG channels and remove mastoid 
-last_channel = find(strncmp('EEG P4',labels,6));
-channels = 1 : last_channel;
-mastoid_A1 = find(strncmp('EEG A1',labels(channels),6));
-if ~isempty(mastoid_A1)
-    channels = [channels(1 : mastoid_A1-1) channels(mastoid_A1+1 : end)];
-end
-mastoid_A2 = find(strncmp('EEG A2',labels(channels),6));
-if ~isempty(mastoid_A2)
-    channels = [channels(1 : mastoid_A2-1) channels(mastoid_A2+1 : end)];
-end
-channelList = labels(channels);
-
-dat(1,max(channels)+1:end)=cell(1);
-%% preprocessing per channel
-myCh = 1;
-for i = 1:length(channels)
-    ch = channels(i);
-    raw_data = double(dat{1,ch}).*scle(ch)-offs(ch); %Scaling the signal to microvolts
-    
-    % first stage artifact removal (thresholding)
-    try
-        [artifactfree, artifact_data(myCh,:)] = firstStageAR(raw_data,fs);
-    catch ME
-        warning('Could NOT processs artefact removal.')
-        prompt = 'Continue without artefact rejection? Y/N: \n';
-        str = input(prompt,'s');
-        if isempty(str) || strcmp(str, 'Y') ~= 1
-            str = 'N';
-        end
-        if strcmp(str, 'N') == 1
-            rethrow(ME)
-        else
-            artifactfree = raw_data;
-        end
+% first stage artifact removal (thresholding)
+try
+    [artifactfree, artifact_data] = firstStageAR(raw_data,fs,FLGbipolar);
+catch ME
+    warning('Could NOT processs artefact removal.')
+    prompt = 'Continue without artefact rejection? Y/N: \n';
+    str = input(prompt,'s');
+    if isempty(str) || strcmp(str, 'Y') ~= 1
+        str = 'N';
     end
-    clear raw_data
-        
-    if(~all(isnan(artifactfree)))
-        
-        % filtering
-        % replace NaNs
-        [artifactfree,inans]=naninterp(artifactfree);
-        
-        % filtering
-        filtered_data = filtfilt(bn,an,artifactfree);
-        clear artifactfree
-        filtered_data = filtfilt(bb,ab,filtered_data);
-        
-        % swap back in the NaNs:
-        if(~isempty(inans))
-            filtered_data(inans) = NaN;
-        end
-        
-        % Resampling with new frequency
-        data = resample(filtered_data,fs_new,fs); %New sampling frequency will be fs_new Hz
-        clear filtered_data
-        eeg_data(myCh,:) = data';
-        clear data
+    if strcmp(str, 'N') == 1
+        rethrow(ME)
     else
-        eeg_data = [];
-        break
+        artifactfree = raw_data;
     end
-    
-    myCh = myCh + 1;
-    dat(1,ch)=cell(1);
 end
-clear dat
+clear raw_data
 
-% percentage of artifact in each channel
-ArtifactPercPerCh = sum(artifact_data,2) ./ length(artifact_data(1,:));
+if(~all(isnan(artifactfree)))
 
-%% to see filtered data
-% x = eeg_data(1,:);
-% y = fft(x);
-% 
-% n = length(x);          % number of samples
-% f = (0:n-1)*(fs/n);     % frequency range
-% power = abs(y).^2/n;    % power of the DFT
-% 
-% plot(f,power)
-% xlabel('Frequency')
-% ylabel('Power')
+    % filtering
+    % replace NaNs
+    [artifactfree,inans]=naninterp(artifactfree);
+
+    % filtering
+    filtered_data = filtfilt(bn,an,artifactfree);
+    clear artifactfree
+    filtered_data = filtfilt(bb,ab,filtered_data);
+
+    % swap back in the NaNs:
+    if(~isempty(inans))
+        filtered_data(inans) = NaN;
+    end
+
+    % Resampling with new frequency
+    eeg_data = resample(filtered_data,fs_new,fs); %New sampling frequency will be fs_new Hz
+    clear filtered_data
+else
+    eeg_data = [];
+end
+
+% Artefact rate
+artefact_rate = sum(artifact_data,2) ./ length(artifact_data(1,:));
 
 end
 
@@ -119,21 +155,25 @@ end
 %
 % Syntax: [artifactfree, artifactDetected] = firstStageAR(rawData, Fs)
 %
-% Inputs: 
+% Inputs:
 %     rawData          - Raw EEG data
 %     Fs               - sampling frequency
 %
-% Outputs: 
+% Outputs:
 %     artefactfree     - artefact free signal
 %     artefactDetected - indicating location of detected artefacts
 %
 % Saeed Montazeri M., University of Helsinki
 % Started: 10-11-2019
 %-------------------------------------------------------------------------------
-function [artefactfree, artefactDetected] = firstStageAR(rawData, Fs)
+function [artefactfree, artefactDetected] = firstStageAR(rawData, Fs, FLGbipolar)
 %% set parameters
-ART_HIGH_VOLT   = 500;   % in mirco Vs
-ART_TIME_COLLAR = 1/Fs;  % time collar (in seconds) around high-amplitude artefact
+if ~FLGbipolar
+    ART_HIGH_VOLT   = 500;   % in mirco Vs
+else
+    ART_HIGH_VOLT   = 250;   % in mirco Vs
+end
+ART_TIME_COLLAR = 2/Fs;  % time collar (in seconds) around high-amplitude artefact
 
 ART_ELEC_CHECK = 1;      % minimum length required for electrode check (in seconds)
 
@@ -143,7 +183,7 @@ artefactfree = rawData;
 % 1. electrode-checks (continuous row of zeros)
 x_channel=rawData;
 x_channel(x_channel~=0)=1;
-irem=zeros(1,N);    
+irem=zeros(1,N);
 [lens,istart,iend]=len_cont_zeros(x_channel,0);
 ielec=find(lens>=(ART_ELEC_CHECK*Fs));
 
@@ -156,14 +196,14 @@ end
 
 % 2. high-amplitude artefacts
 art_coll=round(ART_TIME_COLLAR*Fs);
-irem=zeros(1,N);     
+irem=zeros(1,N);
 
 thres_upper=ART_HIGH_VOLT;
 ihigh=find(abs(rawData-nanmean(rawData))>thres_upper);
 if(~isempty(ihigh))
     for p=1:length(ihigh)
         irun=(ihigh(p)-art_coll):(ihigh(p)+art_coll);
-        irun(irun<1)=1;  irun(irun>N)=N;               
+        irun(irun<1)=1;  irun(irun>N)=N;
         irem(irun)=1;
     end
 end
@@ -180,7 +220,7 @@ nf_f_low = ElectricFreq-0.5;
 % high-pass frequency
 nf_f_high = ElectricFreq+0.5;
 % order
-order = 6; 
+order = 6;
 % ripple factor
 RippleF = 80;
 % Chebyshev II filter used
@@ -194,7 +234,7 @@ bp_f_low = 0.5;
 % low-pass frequency
 bp_f_high = 35;
 % order
-order = 5; 
+order = 5;
 % ripple factor
 RippleF = 80;
 % Chebyshev II filter used
@@ -224,13 +264,13 @@ end
 
 iOthernans=find(isnan(X));
 if(isempty(iOthernans))
-  return;
+    return;
 elseif(length(iOthernans)==1)
-  if(iOthernans>1)
-    X(iOthernans)=X(iOthernans-1);
-  else
-    X(iOthernans)=X(iOthernans+1);
-  end
+    if(iOthernans>1)
+        X(iOthernans)=X(iOthernans-1);
+    else
+        X(iOthernans)=X(iOthernans+1);
+    end
 else
     try
         X(iOthernans)=interp1(find(~isnan(X)), X(~isnan(X)), iOthernans, method);
